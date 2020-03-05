71234e15897ff80f1183b2cd680def4b60572fe7
neer2808 <uic.neerajkhanna@gmail.com>
2020-03-05 22:09:57 +0530

    // modify the logic to delete the node

--- src/BinarySearchTreeMain.java
+++ src/BinarySearchTreeMain.java
@@ -2,13 +2,21 @@ public class BinarySearchTreeMain {
   public static void main(String[] args) {
     BinarySearchTreeWithLinkedList obj = new BinarySearchTreeWithLinkedList();
     obj.insert(20);
-
-//    obj.insert(60);
-//    obj.insert(80);
+    obj.insert(10);
+    obj.insert(10);
+    obj.insert(5);
+//    obj.insert(20);
+//    obj.insert(5);
 //    obj.insert(10);
+//    obj.insert(4);
+//    obj.insert(17);
+//    obj.insert(43);
+//    obj.insert(49);
+//    obj.insert(30);
     obj.inordertraversal(obj.root);
-    obj.successor(obj.root);
-    obj.minimum();
+    obj.delete(10);
+    obj.inordertraversal(obj.root);
+
 
   }
 
--- src/BinarySearchTreeWithLinkedList.java
+++ src/BinarySearchTreeWithLinkedList.java
@@ -1,7 +1,5 @@
 public class BinarySearchTreeWithLinkedList {
-
   Node root;
-
   public BinarySearchTreeWithLinkedList() {
     root = null;
   }
@@ -10,38 +8,127 @@ public class BinarySearchTreeWithLinkedList {
   public void inordertraversal(Node troot) {
     if (troot != null) {
       inordertraversal(troot.left);
-      System.out.println(troot.data);
+      System.out.print(" " + troot.data);
       inordertraversal(troot.right);
     }
   }
 
-  public void successor(Node troot) {
-    if (troot == null) {
-      System.out.println("No node available");
-      return;
+  public Node search(int val) {
+    Node current = root;
+    while (current.data != val) {
+      if (val < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current == null) {
+        return null;
+      }
     }
-    if (troot.left == null && troot.right == null) {
-      return;
+    return current;
+  } // end of the method
+
+  public boolean delete(int value) {
+
+    // first search the value which you want to delete and write delete cases
+    Node current = root;
+    Node parent = root;
+    boolean isLeftChild = false;
+    while (current.data != value) {
+      parent = current;
+      if (value < current.data) {
+
+        current = current.left;
+        isLeftChild = true;
+      } else {
+
+        current = current.right;
+        isLeftChild = false;
+      }
+      if (current == null) {
+        return false;
+      }
     }
-    Node temp = troot.right;
-    while (temp.left != null) {
-      temp = temp.left;
+    // search complet
+    // Leaf node deletion case
+    if (current.left == null && current.right == null) {
+      System.out.println("Leaf node deletion case");
+      if (current == root) {
+        root = null;
+      } else if (isLeftChild == true) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
     }
-    System.out.println(temp.data);
+    // Node to be deleted has one child case
+    // Node to be deleted has right child
+    else if (current.left == null) {
+      if (current == root) {
+        root = current.right;
+      } else if (isLeftChild) {
+        parent.left = current.right;
+      } else {
+        parent.right = current.right;
+      }
+    }
+    // Node to be deleted has left child
+    else if (current.right == null) {
+      System.out.println("One left child deletion case");
+      if (current == root) {
+        root = current.left;
+      }
+      // if deleted node is left child
+      else if (isLeftChild) {
+        parent.left = current.left;
+      }
+      // if deleted node is right child
+      else {
+        parent.right = current.left;
+      }
+    }
+    // Node to be deleted has two children case
+    else {
+      // find in-order successor of the node to be deleted
+      Node successor = successor(current);
+      if (current == root) {
+        root = successor;
+      } else if (isLeftChild) {
+        parent.left = successor;
+      } else {
+        parent.right = successor;
+      }
+      successor.left = current.left;
+    }
+    return true;
+  }
+
+  public Node successor(Node node) {
+    Node successor = node;
+    Node successorParent = node;
+    Node current = node.right;
+    while (current != null) {
+      successorParent = successor;
+      successor = current;
+      current = current.left;
+    }
+    if (successor != node.right) {
+      successorParent.left = successor.right;
+
+      successor.right = node.right;
+    }
+    return successor;
   }
 
   public void minimum() {
     Node current = root;
-    Node last;
-
+    Node parent = null;
     while (current != null) {
-      last = current;
+      parent = current;
       current = current.left;
     }
-    System.out.println("Minimumm value is " + current.data);
+    System.out.println("Minimumm value is " + parent.data);
   }
-
-
   public void insert(int value) {
     Node newnode = new Node();
     newnode.data = value;
@@ -66,11 +153,8 @@ public class BinarySearchTreeWithLinkedList {
             return;
           }
         }
-
       }
-
     }
-
   }
 }
 
